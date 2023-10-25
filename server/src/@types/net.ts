7c116922ef9c5cfc317cc03dcc05623b7b11252f
AXwain <5733365+axwain@users.noreply.github.com>
2023-10-24 22:03:28 -0500

    Added command and net types

---
+++ server/src/@types/net.ts
@@ -0,0 +1,4 @@
+export type NetData = {
+    type: string
+}
+
---
+++ server/src/net/commands.ts
@@ -0,0 +1,10 @@
+export enum Commands {
+    create =  'createRoomCmd',
+    join = 'joinRoomCmd',
+    ready = 'lobbyReadyCmd',
+    updateRuling = 'updateRulingCmd',
+    playCard = 'playCardCmd',
+    drawCard = 'drawCardCmd',
+    pass = 'turnPassCmd',
+    selectSuit = 'selectSuitCmd'
+}
