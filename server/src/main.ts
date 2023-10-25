1b5dd2adef1cdfcf21e76b9f319a75bce6520309
AXwain <5733365+axwain@users.noreply.github.com>
2023-10-24 22:06:02 -0500

    WebSocket server update

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,5 +1,10 @@
 import { WebSocketServer } from 'ws'
 import { v4 as uuidV4 } from 'uuid'
+import type { CreateRoomCmd } from './@types/commands'
+import type { RegisteredMsg, RoomCreatedMsg } from './@types/messages'
+import type { NetData } from './@types/net'
+import { Commands } from './net/commands'
+import { codeGen } from './utils/codeGen'
 
 console.log('Starting server...')
 const server = new WebSocketServer({
@@ -12,6 +17,7 @@ const address = `${server.options.host}:${server.options.port}`
 console.log(`Server listening on: ${address}`)
 
 const idMap = new Map<string, string>()
+const playerRoomMap = new Map<string, string>()
 
 server.on('connection', (ws, request) => {
     console.log(`Connected from origin: ${request.headers.origin}`)
@@ -22,14 +28,38 @@ server.on('connection', (ws, request) => {
             console.log('invalid data')
             return
         }
-        console.log(data)
+        console.log(data.toString())
+        try {
+            const request = JSON.parse(data.toString()) as NetData
+            if (request.type === Commands.create) {
+                const command = request as CreateRoomCmd
+                const isPlayerInRoom = playerRoomMap.has(command.clientId)
+                if (!isPlayerInRoom) {
+                    const message: RoomCreatedMsg = {
+                        type: 'roomCreated',
+                        roomId: codeGen()
+                    }
+                    playerRoomMap.set(command.clientId, message.roomId)
+                    ws.send(JSON.stringify(message))
+                    return
+                }
+            }
+        } catch (e) {
+            console.error('invalid data', e)
+            ws.send(JSON.stringify({ type: 'error' }))
+        }
     })
 
     const clientId = uuidV4()
     const playerId = uuidV4()
     idMap.set(clientId, playerId)
+    const message: RegisteredMsg = {
+        type: 'registered',
+        clientId,
+        playerId
+    }
 
-    ws.send(JSON.stringify({ clientId, playerId }))
+    ws.send(JSON.stringify(message))
     console.log(`Registered ${clientId}`)
 })
 
