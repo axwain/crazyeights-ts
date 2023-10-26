127f6be98f4194995756287170f3cd9faafc7cd2
AXwain <5733365+axwain@users.noreply.github.com>
2023-10-25 23:00:29 -0500

    Added more net types

--- server/src/@types/net.ts
+++ server/src/@types/net.ts
@@ -1,4 +1,14 @@
+import type WebSocket from "ws"
+import type { Room } from '../core/room'
+import type { Player } from "./game"
+
 export type NetData = {
     type: string
 }
+
+export type ClientPlayerMap = Map<string, Player>
+
+export type PlayerRoomMap = Map<string, Room>
+
+export type RoomWebSocketMap = Map<string, WebSocket>
 
